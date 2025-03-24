8f832dd11d2d82e082d8f13026c51db99104641c
nikhil19-code <jnikhil1919@gmail.com>
2025-03-25 00:43:45 +0530

    Update models.py

--- app/models.py
+++ app/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 
-
 class Student(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     date_of_birth = models.DateField()
     grade = models.IntegerField()
-    phone = models.CharField(max_length=100)
-    email = models.CharField(max_length=100)
+    phone = models.CharField(max_length=20)
+    email = models.EmailField(unique=True)
+
+    class Meta:
+        ordering = ['id']
